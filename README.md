70051a04264fb7feea3a099f15bc8f94eff42aa2
Nicholas de Jong <contact@nicholasdejong.com>
2019-01-06 18:44:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# Keybase Network Graph
+
+Generate a [GraphML](http://graphml.graphdrawing.org/) graph datafile representing the [Keybase.io](https://keybase.io) 
+network based on an origin Keybase user id value.
+
+### Usage
+**Step 1:** Determine the user id (uid) you wish to center the network graph data collection around.  This is easily 
+achieved by visiting [Keybase.io](https://keybase.io) for the user, reviewing any element in the user **chain** and 
+finding the `uid` value from the JSON payload data shown.
+
+**Step 2:** Run `keybase-network-graph.py` as per the example below, start small `--depth=1` since the networks can 
+blow out very large very quickly.  The tool is single threaded so it will take some time collecting all the required
+data.  Data is stored in the `--path` so no Keybase data should be requested more than once. 
+
+**Step 3:** Load the output GraphML file in Gephi (like Photoshop for graphs) - GraphML has the broadest set of 
+graph features supported in Gephi hence the data-format choice.
+
+**Step 4:** Explore the data set...
+
+
+### Example
+```commandline
+$ ./keybase-network-graph.py --uid 3195b4ddeb6f4c4250ab2b5ff7c68b19 --depth 1
+
+processing:3195b4ddeb6f4c4250ab2b5ff7c68b19 depth:0 uid_total:1
+complete:3195b4ddeb6f4c4250ab2b5ff7c68b19 followers_added:17
+...
+processing:96ef480ac888bdff3ecb2a8f6e368b00 depth:1 uid_total:326
+complete:96ef480ac888bdff3ecb2a8f6e368b00 followers_added:0
+
+GraphML file: /tmp/keybase-network-graph/3195b4ddeb6f4c4250ab2b5ff7c68b19.graphml
+```  
+
+### Command Line Arguments
+```commandline
+$ ./keybase-network-graph.py -h
+usage: keybase-network-graph.py [-h] --uid <uid> [--depth <depth>]
+                                [--path <path>] [--nograph]
+Keybase Network Grapher
+
+optional arguments:
+  -h, --help       show this help message and exit
+  --uid <uid>      Initial uid to start collecting data on, required.
+  --depth <depth>  Maximum depth of user connections to collect data on,
+                   default = 1
+  --path <path>    Maximum depth of user connections to collect data down to,
+                   default = /tmp/keybase-network-graph
+  --nograph        Prevent GraphML generation.
+```
+
+### Gephi Hints
+**Learn to use the "Ego Filter"**
+
+An Ego Filter allows you to filter out the noise from a graph focusing in on just one node, this helps focus your 
+visualisations - [article here on Ego Filters](https://markhneedham.com/blog/2012/04/30/gephi-centring-a-graph-around-an-individual-node/)
+
+**Use the username label**
+
+The `username` label can be enabled using the icons at the bottom of the visualisation, the following Youtube video may
+help - https://www.youtube.com/watch?v=eu1mibcOrN0
+
+**Other Gephi tutorials**
+
+The following Gephi tutorial is also quite helpful - https://www.youtube.com/watch?v=dSx5_PjaWVE
+
+**Issues with Gephi on Ubuntu**
+
+Ubuntu 18.04 (and 18.10) does not run Gephi out of the box due to an incompatible default version of Java.  This can be
+resolved by installing `openjdk-8-jdk` and updating the `alternatives` configuration as such
+```commandline
+sudo apt-get install openjdk-8-jdk
+
+sudo update-java-alternatives --list
+
+sudo update-alternatives --config javac
+sudo update-alternatives --config java
+```
+
+### Gephi Visualization Samples
+![screenshot_182742.png](assets/screenshot_182742.png)
+
+![screenshot_183251.png](assets/screenshot_183251.png)
---
+++ keybase-network-graph.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import time
+import argparse
+import requests
+from pygraphml import Graph
+from pygraphml import GraphMLParser
+from bs4 import BeautifulSoup
+
+
+class KeybaseNetworkGraphException(Exception):
+    pass
+
+
+class KeybaseNetworkGraph:
+
+    name = 'Keybase Network Grapher'
+    sleep_interval = 0.01
+
+    args = None
+    graph = None
+    data_path = None
+    max_depth = None
+
+    data = {}
+
+    def __init__(self):
+        parser = argparse.ArgumentParser(description=self.name)
+        parser.add_argument('--uid', type=str, metavar='<uid>', required=True,
+                            help='Initial uid to start collecting data on, required.')
+        parser.add_argument('--depth', type=int, metavar='<depth>', default=1,
+                            help='Maximum depth of user connections to collect data on, default = 1')
+        parser.add_argument('--path', type=str, metavar='<path>', default='/tmp/keybase-network-graph',
+                            help='Maximum depth of user connections to collect data down to, default = /tmp/keybase-network-graph')
+        parser.add_argument('--nograph', action='store_true', default=False,
+                            help='Prevent GraphML generation.')
+        self.args = parser.parse_args()
+
+    def main(self):
+        self.data_path = self.args.path
+        self.max_depth = self.args.depth
+
+        if self.args.nograph is False:
+            self.graph = Graph()
+
+        uid = self.args.uid
+        while uid is not None:
+            self.process_uid(uid)
+            time.sleep(self.sleep_interval)
+            uid = self.find_next_uid()
+
+        if self.graph is not None:
+            graph_parser = GraphMLParser()
+            graph_filename = os.path.join(self.data_path, self.args.uid + '.graphml')
+            graph_parser.write(self.graph, graph_filename)
+            self.out('GraphML file: {}'.format(graph_filename))
+
+    def find_next_uid(self):
+        for uid in self.data.keys():
+            if 'followers' not in self.data[uid] or self.data[uid]['followers'] is None:
+                if self.data[uid]['depth'] <= self.max_depth:
+                    return uid
+            elif 'userdata' not in self.data[uid] or self.data[uid]['userdata'] is None:
+                if self.data[uid]['depth'] <= self.max_depth:
+                    return uid
+        return None
+
+    def process_uid(self, uid, use_datastore=True):
+
+        if uid not in self.data:
+            self.data[uid] = {}
+        if 'depth' not in self.data[uid]:
+            self.data[uid]['depth'] = 0
+        if 'graph_node' not in self.data[uid] and self.graph is not None:
+            self.data[uid]['graph_node'] = self.graph.add_node(uid)
+
+        self.out('processing:{} depth:{} uid_total:{}'.format(uid, self.data[uid]['depth'], len(self.data.keys())))
+
+        if self.data[uid]['depth'] > self.max_depth:
+            self.out('max_depth_limited:{} '.format(uid))
+            return
+
+        userdata, self.data[uid]['userdata'] = self.get_userdata(uid, datastore=use_datastore)
+        self.data[uid]['username'] = userdata['userdata']['them'][0]['basics']['username']
+
+        if 'graph_node' in self.data[uid]:
+            self.data[uid]['graph_node']['username'] = self.data[uid]['username']
+
+        followers_added = 0
+        followers, self.data[uid]['followers'] = self.get_followers(uid, datastore=use_datastore)
+        for follower_uid in followers['uids_flattened']:
+            if follower_uid not in self.data:
+                self.data[follower_uid] = {}
+                followers_added += 1
+            if 'depth' not in self.data[follower_uid]:
+                self.data[follower_uid]['depth'] = self.data[uid]['depth'] + 1
+            if 'graph_node' not in self.data[follower_uid] and self.graph is not None:
+                self.data[follower_uid]['graph_node'] = self.graph.add_node(follower_uid)
+            if self.graph is not None:
+                self.graph.add_edge(self.data[uid]['graph_node'], self.data[follower_uid]['graph_node'])
+
+        self.out('complete:{} followers_added:{}'.format(uid, followers_added))
+        self.out('===')
+
+    def get_uid_datapath(self, uid, make_path=False):
+        path = os.path.join(self.data_path, uid[0:2], uid[2:4])
+        if make_path is True and not os.path.isdir(path):
+            os.makedirs(path, exist_ok=True)
+        return path
+
+    def get_uid_followers_filename(self, uid):
+        return os.path.join(self.get_uid_datapath(uid), uid) + '_followers.json'
+
+    def get_uid_userdata_filename(self, uid):
+        return os.path.join(self.get_uid_datapath(uid), uid) + '_userdata.json'
+
+    def get_followers(self, uid, datastore=True):
+        datastore_filename = self.get_uid_followers_filename(uid)
+        if datastore is True and os.path.isfile(datastore_filename):
+            with open(datastore_filename) as f:
+                return json.load(f), datastore_filename
+
+        data = {
+            'followers': self.request_followers(uid=uid, reverse=0),
+            'following': self.request_followers(uid=uid, reverse=1),
+            'uids_flattened': [],
+            'timestamp': int(time.time()),
+        }
+
+        data['uids_flattened'].extend([
+            d['uid'] for d in data['followers'] if ('uid' in d and d['uid'] not in data['uids_flattened'])
+        ])
+        data['uids_flattened'].extend([
+            d['uid'] for d in data['following'] if ('uid' in d and d['uid'] not in data['uids_flattened'])
+        ])
+
+        if datastore is True:
+            self.get_uid_datapath(uid, make_path=True)
+            with open(datastore_filename, 'w') as f:
+                json.dump(data, f)
+        return data, datastore_filename
+
+    def get_userdata(self, uid, datastore=True):
+        datastore_filename = self.get_uid_userdata_filename(uid)
+        if datastore is True and os.path.isfile(datastore_filename):
+            with open(datastore_filename) as f:
+                return json.load(f), datastore_filename
+
+        data = {
+            'userdata': self.request_userdata(uid=uid),
+            'timestamp': int(time.time())
+        }
+        if datastore is True:
+            self.get_uid_datapath(uid, make_path=True)
+            with open(datastore_filename, 'w') as f:
+                json.dump(data, f)
+        return data, datastore_filename
+
+    def request_followers(self, uid, reverse, num=100):
+        url = 'https://keybase.io/_/api/1.0/user/load_more_followers.json'
+        params = {
+            'reverse': int(reverse),
+            'uid': uid,
+            'last_uid': uid,
+            'num_wanted': num
+        }
+
+        r = requests.get(url, params)
+        if r.status_code != 200:
+            raise KeybaseNetworkGraphException('request_followers() received a non http 200 response', r.content)
+
+        if 'snippet' not in r.json():
+            raise KeybaseNetworkGraphException('request_followers() did not respond with expected snippet data', r.content)
+        else:
+            snippet = r.json()['snippet']
+
+        soup = BeautifulSoup(snippet, 'html.parser')
+
+        followers = []
+        for follower in soup.find_all('tr'):
+            if not follower.find('a', class_='username'):
+                continue
+            user = {
+                'uid': follower.get('data-uid'),
+                'username': follower.find('a', class_='username').get_text(),
+                'name': follower.find('span', class_='small').get_text(),
+                'image': follower.find('img', class_='img-circle').get('src'),
+            }
+            followers.append(user)
+
+        return followers
+
+    def request_userdata(self, uid):
+        url = 'https://keybase.io/_/api/1.0/user/lookup.json'
+        params = {
+            'uids': uid,
+        }
+
+        r = requests.get(url, params)
+        if r.status_code != 200:
+            raise KeybaseNetworkGraphException('request_userdata() received a non http 200 response', r.content)
+
+        return r.json()
+
+    def out(self, message):
+        print(message)
+
+
+KeybaseNetworkGraph().main()
